b68b2af18c75585c772f8ab56b71043a11957ee8
jin-take <fgdaojebvoa237@gmail.com>
2024-09-22 19:49:38 +0900

    クエリの修正

--- backend/repository/articles_by_category_repository.go
+++ backend/repository/articles_by_category_repository.go
@@ -19,9 +19,14 @@ func NewArticlesByCategoryRepository(db *gorm.DB) IArticlesByCategoryRepository 
 }
 
 func (acr *articlesByCategoryRepository) GetArticlesByCategory(categoryID int, articles *[]model.Article) error {
-	if err := acr.db.Joins("inner join article_category_maps acm on acm.article_id = articles.id").
+	if err := acr.db.
+		Table("articles ar").
+		Select("ar.*, ca.*, tags.*").
+		Joins("inner join article_category_maps acm on acm.article_id = ar.id").
 		Joins("inner join categories ca on ca.id = acm.category_id").
+		Joins("inner join tags on tags.id = ar.id").
 		Where("ca.id = ?", categoryID).
+		Preload("Categories").
 		Preload("Tags").
 		Find(articles).Error; err != nil {
 		return err
